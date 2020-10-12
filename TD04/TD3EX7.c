2b403f9771e1531f39cdbf30d345beacdb3abe26
jo2904 <josephdegaullier@gmail.com>
2020-10-12 04:23:56 -0400

    MAJ

--- TD04/TD3EX7.c
+++ TD04/TD3EX7.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include <math.h>
-#include <string.h>
-#include <stdlib.h>
 
 int Miror(int N){
+  int b, n=0;
+  while(N>0){
+    b= N%10;
+    N= (N-b)/10;
+    n= (n*10)+b;
+  }
+  return n;
+}
 
+int NbDigit(int N){
+  int b, i=0;
+  while(N>0){
+    b= N%10;
+    N= (N-b)/10;
+    i +=1;
+  }
+  return i;
+}
 
-
+int Palindromize(int N){
+  int N2;
+  N2 = N * pow(10, NbDigit(N));
+  N2 += Miror(N);
+  return N2;
 }
 
 int main(){
@@ -14,4 +33,6 @@ int main(){
   printf("N= ");
   scanf("%d", &N);
   printf("%d\n", Miror(N));
+  printf("%d\n", NbDigit(N));
+  printf("%d\n", Palindromize(N));
 }
