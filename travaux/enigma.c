4329a823f65d0b47ac7fc5afd4cfbbcf28923b58
jo2904 <59180897+jo2904@users.noreply.github.com>
2020-11-25 14:54:14 +0100

    Update enigma.c

--- travaux/enigma.c
+++ travaux/enigma.c
@@ -1,26 +1,82 @@
 #include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 
-char permut_id[26] = "abcdefghiklmnopqrstuvwxyz";
+char connexions[26] = "abcdefghijklmnopqrstuvwxyz";
+char textRotor[3][26] = {{"EKMFLGDQVZNTOWYHXUSPAIBRCJ"}, {"AJDKSIRUXBLHWTMCQGZNPYFVOE"},{"BDFHJLCPRTXVZNYEIWGAKMUSQO"}};
+char textReflector[26] = "EJMZALYXVBWFCRQUONTSPIKHGD"
+int rotor[3][26];
+int reflector[26];
+int decallage[3] = {0,0,0};
 
-int Encrypt(char lettre) {
-  return lettre - 97;
+int Encrypt(char lettre){
+  int chiffre = lettre-97; //transfo en chiffre
+  chiffre = connexions[chiffre]-97; //passage par cable
+
+  chiffre = rotor[0][chiffre+25-decallage[0]]-25+decallage[0];
+  chiffre = rotor[1][chiffre+25-decallage[1]]-25+decallage[1];
+  chiffre = rotor[2][chiffre+25-decallage[2]]-25+decallage[2];
+
+
+
+
+  decallage[0]++;             //decallage rotor
+  if( decallage[0] == 26){
+    decallage[1]++;
+    decallage[0] =0;
+  }
+  if(decallage[1] == 26){
+    decallage[2]++;
+    decallage[1]=0;
+  }
+  if(decallage[2] == 26)
+    decallage[2] =0;
+
+  return chiffre;
 }
 
-int Permutation(int lettre, int i){
-  return (lettre+25-i)-25+1;
+void Text2Value(){
+  for(int i=0; i<3; i++){
+    for(int j =0; j<26; j++){
+      rotor[i][j] = textRotor[i][j]-65;
+      }
+  }
+  for(int i =0; i<26; i++){
+    reflector[i] = textReflector[i]-65
+  }
 }
 
-int main(){
-  char string[20] ="test";
+void ConnectCables(){
+  char test[12] ="";
+  int diff = 0;
+  do{
+    diff =0;
+    printf("Cables à inversés:\n ex: abcdefghijkl\n");
+    scanf("%12s", test);
+    for(int i=0; i<12; i++){
+      for(int j =0; j<12; j++){
+        if(test[i] == test[j] && i!=j){
+          printf("! deux caractères sont identiques !\n");
+          diff ++;
+        }
+      }
+    }
+  }while (diff != 0);
+  for(int i=0; i<12; i=i+2){
+    connexions[test[i]-97] = test[i+1];
+    connexions[test[i+1]-97] = test[i];
+  }
+}
+
+int main() {
+  char string[] = "test";
   int taille = strlen(string);
 
-  char permut[6][2];
-  for(int i = 0; i<6; i++){
-    scanf("%s %s",  &permut[i][0], &permut[i][1]);
+  ConnectCables();
+  Text2Value();
+  for(int h = 0; h<taille; h++){
+    printf("%c ", Encrypt(string[h])+97);
   }
 
-  for(int j = 0; j<taille ; j++){
-    printf("%d ", Encrypt(string[j]));
-  }
+  return 0;
 }
