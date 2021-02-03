c7437576c4073699e1069c2efb081e67f5684507
jo2904 <josephdegaullier@gmail.com>
2021-02-03 15:23:29 +0100

    MAJ

--- TD11/TD11EX10.c
+++ TD11/TD11EX10.c
@@ -2,17 +2,14 @@
 #include <stdlib.h>
 #include <math.h>
 
-int Hanoi(int N, int n, int *T1[], int *T2[], int *T3[]){
-  if(n == N){
+int Hanoi(int N, int T1, int T2, int T3){
+  if(0 == N){
     return 0;
   }
-  return Honoi(N, n+1, T1, T2, T3);
+  return Honoi(N-1, T1, T2, T3);
 }
 
 int main{
-  int T2[] =  {3, 2, 1, -1, -1, -1, -1};
-  int T2[] =  {-1, -1, -1, -1, -1, -1, -1};
-  int T3[] =  {-1, -1, -1, -1, -1, -1, -1};
 
-  Hanoi(3, 0, T1, T2, T3);
+  Hanoi(3, 1, 2, 3);
 }
