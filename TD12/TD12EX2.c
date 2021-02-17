a0ebb313bc7efc990565faa15786ace402023b27
jo2904 <josephdegaullier@gmail.com>
2021-02-17 09:28:59 +0100

    Maj

--- TD12/TD12EX2.c
+++ TD12/TD12EX2.c
@@ -4,49 +4,59 @@
 #include <time.h>
 
 
-int taille(int tableau[]){
-  int i=0;
-  while(tableau[i] != '\0'){
-    i++;
-  }
-  return i;
+#include <stdio.h>
+#include <stdlib.h>
+
+/*   partitionner()
+ [IN/OUT] int *t : pointeur sur le début du tableau
+ [IN]     int G  : indice de début de tableau
+ [IN]     int D  : indice de fin de tableau
+ La fonction partitionne le tableau autour de la valeur t[G] entre les indices G et D
+ La fonction retourne l'indice du pivot une fois le tableau partitionné.
+ */
+int partitionner(int *tableau, int G, int D)
+{
+    int pivot, i, j;
+    pivot = G;
+    i = G;
+    j = D;
+
+    while(i<j){
+        if(tableau[j] < tableau[i]){
+            int temp = tableau[i];
+            tableau[i] = tableau[j];
+            tableau[j] = temp;
+            if(pivot == i){
+                pivot = j;
+            }
+            else if(pivot == j){
+                pivot = i;
+            }
+        }
+        if(pivot == i){
+            j--;
+        }
+        if(pivot == j){
+            i++;
+        }
+    }
+    return pivot;
 }
 
-void tri_rapide(int *tableau, int deb, int fin){
-  int pivot, i, j;
-  pivot = deb;
-  i = deb;
-  j = fin;
-
-  while(i<j){
-    if(tableau[j] < tableau[i]){
-      int temp = tableau[i];
-      tableau[i] = tableau[j];
-      tableau[j] = temp;
+/*   triRapide()
+ [IN/OUT] int *t : pointeur sur le début du tableau
+ [IN]     int G  : indice de début de tableau
+ [IN]     int D  : indice de fin de tableau
+ La fonction trie le tableau entre les indices G et D
+ La fonction retourne 0 si tout s'est bien passé, -1 en cas d'erreur.
+ */
+int triRapide(int *tableau, int deb, int fin){
+    if(deb >= fin){
+        return 0;
     }
-    if(pivot == i){
-      j--;
-    }
-    if(pivot == j){
-      i++;
-    }
+    int pivot = partitionner(tableau, deb, fin);
+    triRapide(tableau, deb, pivot-1);
+    triRapide(tableau, pivot+1, fin);
 
-    tri_rapide(tableau, deb, pivot-1);
-    tri_rapide(tableau, pivot+1, fin);
-  }
-}
-
-
-int main(){
-  int tableau[] = {4, 11, 8, 5, 6, 2, 1, 0};
-  for(int i = 0; i<8; i++){
-    printf("%d ", tableau[i]);
-  }
-  printf("\n" );
-
-  tri_rapide(tableau, 0, taille(tableau));
-
-  for(int i = 0; i<8; i++){
-    printf("%d ", tableau[i]);
-  }
+    return 0;
 }
