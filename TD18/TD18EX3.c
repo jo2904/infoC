69b9769698d33287ca867abe1b4b461a91642ac6
jo2904 <josephdegaullier@gmail.com>
2021-05-07 08:47:42 +0200

    MAJ

---
+++ TD18/TD18EX3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+  int g = 10, e = 10, ge;1;
+  n = 1;
+  while(e > 0){
+    n = n * g;
+    e--;
+  }
+
+  return n;
+}
